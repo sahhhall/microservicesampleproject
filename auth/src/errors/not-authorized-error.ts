6842bcf8468d637b6810847ecae8d6104243f821
sahhhall <sahalvavoor313@gmail.com>
2024-08-23 15:41:44 +0530

    feat: added middlware for auth

---
+++ auth/src/errors/not-authorized-error.ts
@@ -0,0 +1,13 @@
+import { CustomError } from "./custom-error";
+
+export class NotAuthorizedError extends CustomError {
+  statusCode = 401;
+  constructor(message: string) {
+    super(message);
+    Object.setPrototypeOf(this, NotAuthorizedError.prototype);
+  }
+
+  serializeError() {
+    return [{ message: "Nor Authorized" }];
+  }
+}
