86c5c387fb98999432c2a8cfb1ace45e16df7f47
sahhhall <sahalvavoor313@gmail.com>
2024-08-29 22:10:29 +0530

    feat: intial setup for payment service

---
+++ payments/src/app.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+import cors from "cors";
+import {
+  errorHandler,
+  NotFoundError,
+  curentUser,
+} from "@sahhhalltickets/common";
+
+
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://192.168.49.2:30032", //  all domains
+    credentials: true, // Allow cookies to be sent with requests
+  })
+);
+
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+  })
+);
+
+app.use(curentUser);
+
+
+app.all("*", async (req, res, next) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
